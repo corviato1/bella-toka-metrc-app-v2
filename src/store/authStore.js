e92b96678de3a7cefbf2fef5dabdd39b1a950d63
corviato1 <44780076+corviato1@users.noreply.github.com>
2026-04-19 00:23:45 -0700

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,28 +1,22 @@
 import { create } from 'zustand'
 
 export const useAuthStore = create((set) => ({
-  token: localStorage.getItem('token'),
-  user: JSON.parse(localStorage.getItem('user') || 'null'),
-  isAuthenticated: !!localStorage.getItem('token'),
+  user: null,
+  token: null,
+  isAuthenticated: false,
 
-  login: (data) => {
-    localStorage.setItem('token', data.token)
-    localStorage.setItem('user', JSON.stringify(data.user))
-
+  login: ({ user, token }) => {
     set({
-      token: data.token,
-      user: data.user,
+      user,
+      token,
       isAuthenticated: true,
     })
   },
 
   logout: () => {
-    localStorage.removeItem('token')
-    localStorage.removeItem('user')
-
     set({
-      token: null,
       user: null,
+      token: null,
       isAuthenticated: false,
     })
   },
